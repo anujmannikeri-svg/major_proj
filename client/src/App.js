d5daaf3d416b9728a975dfaf88afd4b5b6762e19
anujmannikeri-svg <anujmannikeri@gamil.com>
2026-04-20 21:17:40 +0530

    connected frontend to backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(process.env.REACT_APP_API_URL + "/api/exams")
+      .then(res => res.json())
+      .then(data => setData(data))
+      .catch(err => console.log(err));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Online Exam System</h1>
+      {data.length === 0 ? (
+        <p>No data yet</p>
+      ) : (
+        data.map((item, index) => (
+          <div key={index}>
+            <p>{item.name}</p>
+          </div>
+        ))
+      )}
     </div>
   );
 }
